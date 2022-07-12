a127e3cb854c3204c3577db6a75fedbc01c8a44e
colejal <jaleelcole215@gmail.com>
2022-07-12 05:29:50 -0400

    This is commit for challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,67 @@
 import os
 import csv
 
-budget_data = os.path.join('..','Resources','budget_data.csv')
 
-with open(budget_data,'r') as csv_file:
-    csv_reader = csv.reader(csv_file,delimiter = ",")
+budget_data = os.path.join("Resources","budget_data.csv")
 
-    csv_header= next(csv_file)
+Revenue = []
+Date = []
+Gains_Change = []
+Change_Monthly = []
+Gains_Change_row = []
+Gains_Average = []
+
+Date = 0
+Gains_Totals = 0
+previous_gains = 0 
+greatest_increase = ["", 0]
+greatest_decrease = ["", 99999999999999999]
+
+
+
+with open(budget_data) as csvfile:
+    csvreader = csv.reader(csvfile,delimiter = ",")
+
+    csv_header= next(csvfile)
     print(f"Header:{csv_header}")
 
-Date_Year=0
-Revenue_Gains_Losses=0
+    for row in csvreader:
+        Date = Date + 1
+        
+        Revenue.append(row[1])
+        Gains_Totals = Gains_Totals + int(row[1])
+           
+        gains_change = int(row[1]) - previous_gains
+        previous_gains = int(row[1])
+        Gains_Change_row = Gains_Change_row +[Gains_Change]
+        Change_Monthly = Change_Monthly + [row[0]]
 
-for row in csv_reader:
-    Date_Year.append(row[0])
-    Revenue_Gains_Losses.append(int(row[1]))
-Date_Total=sum(Date_Year)
+        if (gains_change > greatest_increase[1]):
+            greatest_increase[0] = row[1]
+            greatest_increase[1] = gains_change
+
+        if(gains_change < greatest_decrease[1]):
+            greatest_decrease[0] = row[1]
+            greatest_decrease[1] = gains_change
+
+
+
+
+
+print("Financial Analysis")
+print("-----------------------------------------")
+print(f"Total Months:" +" "+ str(Date))
+print(f"Total:" +" $"+ str(Gains_Totals))
+
+print(f"Greatest Increase in Profits:" +"$"+str(greatest_increase[1])) 
+print(f"Greatest Decrease in Profits:" +"$" +str(greatest_decrease[1]))
+
+with open('financial_analysis.text', 'w') as text:
+    text.write("Financial Analysis")
+    text.write("-----------------------------------------")
+    text.write(f"Total Months:" +" "+ str(Date))
+    text.write(f"Total:" +" $"+ str(Gains_Totals))
+    
+    text.write(f"Greatest Increase in Profits:" +"$"+str(greatest_increase[1])) 
+    text.write(f"Greatest Decrease in Profits:" +"$"+str(greatest_decrease[1]))
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,10 @@
-print("This is main")
+import os
+import csv
+
+election_data = os.path.join("..","resources","election_data.csv")
+
+with open(election_data,'r') as csv_file:
+    csv_reader = csv.reader(csv_file,delimiter = ",")
+
+    csv_header= next(csv_file)
+    print(f"Header:{csv_header}")
